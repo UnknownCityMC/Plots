2e8b0dd922c09c65baace9e65ea3d2e20fa178c5
Wolfbros <jensthe007@gmail.com>
2025-05-25 21:44:33 +0200

    feat: change biome materials

--- src/main/java/de/unknowncity/plots/gui/BiomeChangeGUI.java
+++ src/main/java/de/unknowncity/plots/gui/BiomeChangeGUI.java
@@ -2,23 +2,63 @@ package de.unknowncity.plots.gui;
 
 import de.unknowncity.astralib.paper.api.item.ItemBuilder;
 import de.unknowncity.plots.PlotsPlugin;
-import de.unknowncity.plots.plot.Plot;
 import de.unknowncity.plots.gui.items.BiomeChangeItem;
 import de.unknowncity.plots.gui.util.PagedGUI;
+import de.unknowncity.plots.plot.Plot;
 import de.unknowncity.plots.service.PlotService;
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.block.Biome;
 import org.bukkit.entity.Player;
 import org.spongepowered.configurate.NodePath;
 import xyz.xenondevs.invui.item.Item;
 import xyz.xenondevs.invui.item.impl.SimpleItem;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import static io.papermc.paper.registry.keys.BiomeKeys.*;
+
 public class BiomeChangeGUI {
+
+    final static Registry<Biome> biomeRegistry = RegistryAccess
+            .registryAccess()
+            .getRegistry(RegistryKey.BIOME);
+
+    private static final Map<Biome, Material> biomeBlockMap = new HashMap<>();
+
+
+    static {
+        // Overworld Biomes
+        biomeBlockMap.put(biomeRegistry.get(PLAINS), Material.GRASS_BLOCK);
+        biomeBlockMap.put(biomeRegistry.get(FOREST), Material.OAK_LOG);
+        biomeBlockMap.put(biomeRegistry.get(JUNGLE), Material.JUNGLE_LOG);
+        biomeBlockMap.put(biomeRegistry.get(SAVANNA), Material.ACACIA_LOG);
+        biomeBlockMap.put(biomeRegistry.get(DESERT), Material.SAND);
+        biomeBlockMap.put(biomeRegistry.get(SNOWY_PLAINS), Material.SNOW_BLOCK);
+        biomeBlockMap.put(biomeRegistry.get(MUSHROOM_FIELDS), Material.MYCELIUM);
+        biomeBlockMap.put(biomeRegistry.get(SWAMP), Material.SLIME_BLOCK);
+        biomeBlockMap.put(biomeRegistry.get(MEADOW), Material.TERRACOTTA);
+
+        // Ocean Biomes
+        biomeBlockMap.put(biomeRegistry.get(OCEAN), Material.WATER_BUCKET);
+        biomeBlockMap.put(biomeRegistry.get(COLD_OCEAN), Material.BLUE_ICE);
+        biomeBlockMap.put(biomeRegistry.get(FROZEN_OCEAN), Material.ICE);
+        biomeBlockMap.put(biomeRegistry.get(WARM_OCEAN), Material.BRAIN_CORAL_BLOCK);
+
+        // Nether Biomes
+        biomeBlockMap.put(biomeRegistry.get(NETHER_WASTES), Material.NETHERRACK);
+
+        // The End
+        biomeBlockMap.put(biomeRegistry.get(THE_END), Material.END_STONE);
+    }
 
     public static void open(Player player, Plot plot, PlotsPlugin plugin) {
         var messenger = plugin.messenger();
@@ -29,12 +69,16 @@ public class BiomeChangeGUI {
                 messenger.component(player, NodePath.path("gui", "biome", "item", "back", "name"))
         ).item(), click -> PlotMainGUI.open(player, plot, plugin));
 
-        List<Item> items = Arrays.stream(Biome.values()).map(biome -> new BiomeChangeItem(new xyz.xenondevs.invui.item.builder.ItemBuilder(ItemBuilder.of(Material.GRASS_BLOCK).name(
-                messenger.component(player, NodePath.path("gui", "biome", "item", "biome", "name"), Placeholder.parsed("biome", biome.name()))
+        List<Item> items = biomeBlockMap.keySet().stream().map(biome -> new BiomeChangeItem(new xyz.xenondevs.invui.item.builder.ItemBuilder(ItemBuilder.of(getItemForBiome(biome)).name(
+                messenger.component(player, NodePath.path("gui", "biome", "item", "biome", "name"), Placeholder.component("biome", Component.translatable(biome.translationKey())))
         ).item()), plot, biome, plugin)).collect(Collectors.toList());
 
         var gui = PagedGUI.createAndOpenPagedGUI(messenger, title, backItem, items, player);
         gui.addCloseHandler(() -> plugin.serviceRegistry().getRegistered(PlotService.class).savePlot(plot));
         gui.open();
+    }
+
+    public static Material getItemForBiome(Biome biome) {
+        return biomeBlockMap.getOrDefault(biome, Material.BARRIER);
     }
 }
