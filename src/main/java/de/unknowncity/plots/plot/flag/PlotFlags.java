f16c00cbfebf63383bf1d334d9cd82993144cd80
TheZexquex <jakobreiser@gmx.de>
2025-08-19 14:45:19 +0200

    feat: add flags for snow melt and snow fall

--- src/main/java/de/unknowncity/plots/plot/flag/PlotFlags.java
+++ src/main/java/de/unknowncity/plots/plot/flag/PlotFlags.java
@@ -1,8 +1,6 @@
 package de.unknowncity.plots.plot.flag;
 
-import de.unknowncity.plots.plot.flag.type.block.IceFormFlag;
-import de.unknowncity.plots.plot.flag.type.block.IceMeltFlag;
-import de.unknowncity.plots.plot.flag.type.block.LeafDecayFlag;
+import de.unknowncity.plots.plot.flag.type.block.*;
 import de.unknowncity.plots.plot.flag.type.player.BlockBreakFlag;
 import de.unknowncity.plots.plot.flag.type.player.BlockPlaceFlag;
 import de.unknowncity.plots.plot.flag.type.player.ItemDropFlag;
@@ -18,6 +16,8 @@ public class PlotFlags {
         flagRegistry.registerToCat(PlotFlag.Category.BLOCK, new IceMeltFlag());
         flagRegistry.registerToCat(PlotFlag.Category.BLOCK, new IceFormFlag());
         flagRegistry.registerToCat(PlotFlag.Category.BLOCK, new LeafDecayFlag());
+        flagRegistry.registerToCat(PlotFlag.Category.BLOCK, new SnowFallFlag());
+        flagRegistry.registerToCat(PlotFlag.Category.BLOCK, new SnowMeltFlag());
 
         // Player flags (mostly PlotAccessModifier flags)
         flagRegistry.registerToCat(PlotFlag.Category.PLAYER, new BlockPlaceFlag(plotService));
