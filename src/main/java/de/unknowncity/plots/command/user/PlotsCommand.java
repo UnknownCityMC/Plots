11efd5bb61d8f08b601ea857813911950ad14620
TheZexquex <jakobreiser@gmx.de>
2025-10-15 14:02:40 +0200

    feat: allow admins to open plot overview of other players

--- src/main/java/de/unknowncity/plots/command/user/PlotsCommand.java
+++ src/main/java/de/unknowncity/plots/command/user/PlotsCommand.java
@@ -2,17 +2,17 @@ package de.unknowncity.plots.command.user;
 
 import de.unknowncity.astralib.paper.api.command.PaperCommand;
 import de.unknowncity.plots.PlotsPlugin;
-import de.unknowncity.plots.gui.PlotMainGUI;
 import de.unknowncity.plots.gui.ViewPlotsGUI;
-import de.unknowncity.plots.plot.PlotUtil;
 import de.unknowncity.plots.service.PlotService;
 import de.unknowncity.plots.service.RegionService;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.incendo.cloud.CommandManager;
 import org.incendo.cloud.context.CommandContext;
-import org.spongepowered.configurate.NodePath;
+
+import static de.unknowncity.plots.command.argument.UcPlayerParser.ucPlayerParser;
 
 public class PlotsCommand extends PaperCommand<PlotsPlugin> {
     private final RegionService regionService = plugin.serviceRegistry().getRegistered(RegionService.class);
@@ -30,11 +30,25 @@ public class PlotsCommand extends PaperCommand<PlotsPlugin> {
                 .senderType(Player.class)
                 .handler(this::handle)
         );
+
+        commandManager.command(commandManager.commandBuilder("plots")
+                .permission("plots.command.plots.other")
+                .required("target", ucPlayerParser())
+                .senderType(Player.class)
+                .handler(this::handleOther)
+        );
     }
 
     private void handle(@NonNull CommandContext<Player> context) {
         var sender = context.sender();
 
-        ViewPlotsGUI.open(sender, plugin);
+        ViewPlotsGUI.openSelf(sender, plugin);
+    }
+
+    private void handleOther(@NonNull CommandContext<Player> context) {
+        var sender = context.sender();
+        var target = (OfflinePlayer) context.get("target");
+
+        ViewPlotsGUI.openForOther(sender, target, plugin);
     }
 }
--- src/main/java/de/unknowncity/plots/gui/ViewPlotsGUI.java
+++ src/main/java/de/unknowncity/plots/gui/ViewPlotsGUI.java
@@ -1,33 +1,39 @@
 package de.unknowncity.plots.gui;
 
 import de.unknowncity.plots.PlotsPlugin;
-import de.unknowncity.plots.gui.items.*;
-import de.unknowncity.plots.plot.flag.PlotInteractable;
+import de.unknowncity.plots.gui.items.ManagePlotItem;
+import de.unknowncity.plots.gui.items.NextPageItem;
+import de.unknowncity.plots.gui.items.PrevPageItem;
 import de.unknowncity.plots.service.PlotService;
-import de.unknowncity.plots.service.plot.InteractablesService;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.spongepowered.configurate.NodePath;
 import xyz.xenondevs.invui.gui.Markers;
 import xyz.xenondevs.invui.gui.PagedGui;
 import xyz.xenondevs.invui.gui.Structure;
-import xyz.xenondevs.invui.item.Item;
 import xyz.xenondevs.invui.window.Window;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ViewPlotsGUI {
 
-    public static void open(Player player, PlotsPlugin plugin) {
+    public static void openSelf(Player player, PlotsPlugin plugin) {
+        open(player, player, plugin);
+    }
+
+    public static void openForOther(Player player, OfflinePlayer target, PlotsPlugin plugin) {
+        open(player, target, plugin);
+    }
+
+    private static void open(Player player, OfflinePlayer target, PlotsPlugin plugin) {
         var plotService = plugin.serviceRegistry().getRegistered(PlotService.class);
 
         var title = plugin.messenger().component(player, NodePath.path("gui", "viewplots", "title"));
 
-        var plotsOwned = plotService.findPlotsByOwnerUUID(player.getUniqueId());
-        var plotsMemberOf = plotService.findPlotsByMember(player.getUniqueId());
+        var plotsOwned = plotService.findPlotsByOwnerUUID(target.getUniqueId());
+        var plotsMemberOf = plotService.findPlotsByMember(target.getUniqueId());
 
         var items = Stream.concat(plotsOwned.stream(), plotsMemberOf.stream())
                 .map(plot -> new ManagePlotItem(plugin, plot))
