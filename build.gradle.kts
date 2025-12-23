fe338886ac0481e56eca10ce3dfad9d083be327a
Wolfbros <jensthe007@gmail.com>
2025-12-23 09:47:42 +0100

    fix: revert run paper version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("java")
     id("io.github.goooler.shadow") version "8.1.8"
     id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
-    id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "de.unknowncity"
